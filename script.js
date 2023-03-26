c6d4dfca005b8531d5e8d3d937ca5ac0ab96894a
Emilio <emiliogonlon@outlook.com>
2023-03-27 00:15:44 +0200

    successfully display fetched data on carrousel

--- script.js
+++ script.js
@@ -9,13 +9,23 @@ fetch('https://vildskud-9634.restdb.io/rest/forestillinger', {
 })
 .then(response => response.json())
 .then(data => {
-  let output = '<ul>';
+  let output = '<ul id="output" class="carrousel">';
   data.forEach(function(forestilling) {
     output += `
-      <li>
-        <h3>${forestilling.Navn}</h3>
-        <p>${forestilling.konsulent}</p>
-      </li>
+      <li class="bannerframe">
+        <img src="assets/9BEF0B3D-169C-4CB1-82BF-C3ED7BC468D8.jpeg" alt="Det levende rum poster">
+        <div class="bannertekst">
+        <h3 class="bannertitel">
+            ${forestilling.Navn}
+        </h3>
+        <p class="ekstrainfo">
+            ${forestilling.Dato_1}
+        </p>
+        <p>
+            ${forestilling.short}
+        </p>
+        </div>
+    </li>
     `;
   });
   output += '</ul>';
