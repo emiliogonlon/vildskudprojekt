e6d1153dffe7ed195777025e4aced38d3274a57c
Emilio <emiliogonlon@outlook.com>
2023-03-27 23:25:38 +0200

    burger menu created

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
 console.log("helloWorld")
 
-fetch('https://vildskud-9634.restdb.io/rest/forestillinger', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    'x-apikey': '6420a7ae22634c74fb00b1f5'
+function openNav() {
+    document.getElementById("mySidepanel").style.display = "flex"
+}
+
+function closeNav() {
+    document.getElementById("mySidepanel").style.display = "none"
   }
-})
-.then(response => response.json())
-.then(data => {
-  let output = '<ul id="output" class="carrousel">';
-  data.forEach(function(forestilling) {
-    output += `
-      <li class="bannerframe">
-        <a href="${forestilling.url}">
-            <img src="assets/${forestilling.imageloc}.avif">
-            <div class="bannertekst">
-            <h3 class="bannertitel">
-                ${forestilling.Navn}
-            </h3>
-            <p class="ekstrainfo">
-                ${forestilling.Dato_1}
-            </p>
-            <p>
-                ${forestilling.short}
-            </p>
-            </div>
-        </a>
-    </li>
-    `;
-  });
-  output += '</ul>';
-  document.getElementById('output').innerHTML = output;
-})
-.catch(err => console.log(err));
